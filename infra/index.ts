07d47ef7372199f434749e2aa8ac1d359ae913c9
ele <eleanor.sl.leung@gmail.com>
2023-01-30 12:24:52 +0000

    wip cloud run deployment

--- infra/index.ts
+++ infra/index.ts
@@ -1,5 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
+import * as docker from "@pulumi/docker";
 
 const location = gcp.config.region || "europe-west2";
 
@@ -7,12 +8,19 @@ const enableCloudRun = new gcp.projects.Service("EnableCloudRun", {
     service: "run.googleapis.com",
 });
 
+const ktorSampleImage = new docker.Image("ktor-sample-image", {
+    imageName: pulumi.interpolate`gcr.io/${gcp.config.project}/ktor-sample:v1.0.0`,
+    build: {
+        context: "../",
+    },
+});
+
 const exampleApp = new gcp.cloudrun.Service("ktor-sample", {
     location: "europe-west2",
     template: {
         spec: {
             containers: [{
-                image: "gcr.io/cloudrun/hello",
+                image: ktorSampleImage.imageName,
             }],
         },
     },
