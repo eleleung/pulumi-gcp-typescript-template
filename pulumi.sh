fcb775b8443195fce139f5b8c1fa7cc9d14de70f
ele <eleanor.sl.leung@gmail.com>
2023-02-02 17:15:39 +0000

    test pulumi cloud build integration

---
+++ pulumi.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# exit if a command returns a non-zero exit code and also print the commands and their args as they are executed.
+set -e -x
+
+# Download and install required tools.
+# pulumi
+curl -L https://get.pulumi.com/ | bash
+export PATH=$PATH:$HOME/.pulumi/bin
+
+# Restore npm dependencies for our infra app.
+yarn install
+
+# Login into pulumi. This will require the PULUMI_ACCESS_TOKEN environment variable.
+pulumi login
+
+# Select the appropriate stack.
+pulumi stack select $PULUMI_STACK
+
+case $BUILD_TYPE in
+  PullRequest)
+      pulumi preview
+    ;;
+  *)
+      pulumi up --yes
+    ;;
+esac
